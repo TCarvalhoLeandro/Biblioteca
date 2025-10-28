331fb30c4b3976fe92a23c6f29016c6a7e4a90fe
Leandro <trairicarvalho@gmail.com>
2025-10-28 19:22:55 -0300

    update

--- src/program/Main.java
+++ src/program/Main.java
@@ -3,30 +3,38 @@ package program;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
+
 import service.Biblioteca;
 import service.GerenciaDados;
 
 public class Main {
 
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) throws Exception{
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		Biblioteca biblioteca = new Biblioteca();// Cria o componente de dados
-
-		System.out.println("=".repeat(15) + " BIBLIOTECA DO LEANDRO " + "=".repeat(15));
-
-		System.out.println("[0] - Sair");
-		System.out.println("[1] - Gerenciar livros");
-		System.out.println("[2] - Gerenciar leitores");
-
+		boolean valido = false;
+		while(!valido) {
+			try {	
+				System.out.println("=".repeat(15) + " BIBLIOTECA DO LEANDRO " + "=".repeat(15));
+				System.out.println("[0] - Sair");
+				System.out.println("[1] - Gerenciar livros");
+				System.out.println("[2] - Gerenciar leitores");
+				System.out.println();
+				System.out.print("Escolha uma opção: ");
+				int opcao = Integer.parseInt(br.readLine());
+				
+				// Injeta o componente na classe de serviço/gerenciamento (Composição)
+				GerenciaDados gerenciaLivro = new GerenciaDados(biblioteca);
+				gerenciaLivro.executar(opcao);
+				
+			}
+			catch (Exception e) {
+				System.out.println("Erro: " + e.getMessage());
+			}
+		}
 		System.out.println();
-		System.out.print("Escolha uma opção: ");
-		int opcao = Integer.parseInt(br.readLine());
 		System.out.println("=".repeat(53));
-
-		// Injeta o componente na classe de serviço/gerenciamento (Composição)
-		GerenciaDados gerenciaLivro = new GerenciaDados(biblioteca);
-		gerenciaLivro.executar(opcao);
 
 	}
 
